28dfdb6ac05ea6c96dd60eebb83557ffba8c358f
Tim <timjoco13@gmail.com>
2025-11-08 05:48:57 -0600

    updated redirect for testing

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const isProd =
   process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: { domains: ['images.unsplash.com'] },
 
   async redirects() {
