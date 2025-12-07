49a99ea2d7daa5e2f7b4c22a7fc52f62235c52fd
Tim <timjoco13@gmail.com>
2025-12-06 18:13:25 -0600

    fixes for vercel

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'app.amplee',
   appName: 'Amplee',
   webDir: 'dist',
-  server: { url: 'http://192.168.1.119:5173/', cleartext: true },
+  // server: { url: 'http://192.168.1.119:5173/', cleartext: true },
   plugins: {
     Keyboard: {
       // Let the OS resize the webview natively when keyboard shows
